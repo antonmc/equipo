3423282478ce00fec133c3227d3d449bdb7e3745
antonm <antonm@ca.ibm.com>
2016-05-30 10:53:20 -0400

    Gathering game data

---
+++ gamester.js
@@ -0,0 +1,45 @@
+var request = require('request');
+fs = require('fs');
+var moment = require('moment');
+var cheerio = require('cheerio');
+
+
+var url = 'http://www.ca2016.com/matches';
+
+request(url, function (error, response, html) {
+    if (!error) {
+
+        var set = [];
+
+        var scan = cheerio.load(html);
+
+        var matches = scan('.match')
+
+        var date = matches[0].children[0].next.children[0].data;
+
+        var home = matches[0].children[3].children[2].next.children[1].children[1].children[1].children[0].data
+
+        var away = matches[0].children[3].children[2].next.children[1].children[4].next.children[3].children[0].data;
+
+        console.log('' + home + ' v ' + away + ', ' + date);
+
+        matches.each(function (i, element) {
+
+            var date = element.children[0].next.children[0].data;
+
+            var home = element.children[3].children[2].next.children[1].children[1].children[1].children[0].data
+
+            var away = '';
+
+            if (element.children[3].children[2].next.children[1].children[4] !== undefined) {
+
+
+
+
+                away = element.children[3].children[2].next.children[1].children[4].next.children[3].children[0].data;
+            }
+
+            console.log('' + home + ' v ' + away + ', ' + date);
+        })
+    }
+})
